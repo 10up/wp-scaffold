0df2b643a99844f57692f0cd4405b2e784ec5549
Nícholas André <nicholas.andre@10up.com>
2021-02-24 10:33:52 -0300

    Adding describe block

--- themes/10up-theme/includes/blocks/example-block/__tests__/edit.js
+++ themes/10up-theme/includes/blocks/example-block/__tests__/edit.js
@@ -2,20 +2,22 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Edit from '../edit';
 
-test('it works', () => {
-	const setAttributes = jest.fn();
-	const { container } = render(
-		<Edit setAttributes={setAttributes} attributes={{ currentTitle: 'test' }} />,
-	);
+describe('example block', () => {
+	test('edit component works', () => {
+		const setAttributes = jest.fn();
+		const { container } = render(
+			<Edit setAttributes={setAttributes} attributes={{ currentTitle: 'test' }} />,
+		);
 
-	expect(container).toMatchInlineSnapshot(`
+		expect(container).toMatchInlineSnapshot(`
 		<div>
 		  <div>
 		    <textarea />
 		  </div>
 		</div>
-	`);
+		`);
 
-	userEvent.type(screen.getByRole('textbox'), 'my value');
-	expect(setAttributes).toHaveBeenCalled();
+		userEvent.type(screen.getByRole('textbox'), 'my value');
+		expect(setAttributes).toHaveBeenCalled();
+	});
 });
