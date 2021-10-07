44b5f1261fcb3ae5d23ad914aeac7bbdae73ac04
Fabian Kägy <mail@fabian-kaegy.de>
2021-10-07 10:01:24 +0200

    add explenation for block-templates removal

--- themes/10up-theme/includes/core.php
+++ themes/10up-theme/includes/core.php
@@ -61,6 +61,10 @@ function theme_setup() {
 	);
 
 	remove_theme_support( 'core-block-patterns' );
+
+	// by adding the `theme.json` file block templates automatically get enabed.
+	// because the template editor will need additional QA and work to get right
+	// the default is to disable this feature.
 	remove_theme_support( 'block-templates' );
 
 	// This theme uses wp_nav_menu() in three locations.
