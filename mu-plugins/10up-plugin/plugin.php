827df67a22e85b61f3b8d75a443f65edde9f29f0
Nícholas Oliveira <nicholas.andre@10up.com>
2022-02-28 19:12:09 -0300

    WIP: HMR set up

--- mu-plugins/10up-plugin/plugin.php
+++ mu-plugins/10up-plugin/plugin.php
@@ -6,6 +6,12 @@ define( 'TENUP_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'TENUP_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
 define( 'TENUP_PLUGIN_INC', TENUP_PLUGIN_PATH . 'includes/' );
 
+if ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG && file_exists( __DIR__ . '/dist/fast-refresh.php' ) ) {
+	define( 'TENUP_TOOLKIT_DIST_URL', TENUP_PLUGIN_URL . '/dist/' );
+	define( 'TENUP_TOOLKIT_DIST_PATH', TENUP_PLUGIN_PATH . '/dist/' );
+	require_once __DIR__ . '/dist/fast-refresh.php';
+}
+
 // Require Composer autoloader if it exists.
 if ( file_exists( TENUP_PLUGIN_PATH . 'vendor/autoload.php' ) ) {
 	require_once TENUP_PLUGIN_PATH . 'vendor/autoload.php';
