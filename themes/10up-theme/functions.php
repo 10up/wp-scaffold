121061dc98977defea6113c12d2e80c326392b05
Nícholas Oliveira <nicholas.andre@10up.com>
2022-02-28 23:57:55 -0300

    Removing unnecessary code

--- themes/10up-theme/functions.php
+++ themes/10up-theme/functions.php
@@ -14,12 +14,6 @@ define( 'TENUP_THEME_DIST_URL', TENUP_THEME_TEMPLATE_URL . '/dist/' );
 define( 'TENUP_THEME_INC', TENUP_THEME_PATH . 'includes/' );
 define( 'TENUP_THEME_BLOCK_DIR', TENUP_THEME_INC . 'blocks/' );
 
-if ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG && file_exists( __DIR__ . '/dist/fast-refresh.php' ) ) {
-	define( 'TENUP_TOOLKIT_DIST_URL', TENUP_THEME_DIST_URL );
-	define( 'TENUP_TOOLKIT_DIST_PATH', TENUP_THEME_DIST_PATH );
-
-	require_once __DIR__ . '/dist/fast-refresh.php';
-}
 
 if ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG && file_exists( __DIR__ . '/dist/fast-refresh.php' ) ) {
 	require_once __DIR__ . '/dist/fast-refresh.php';
