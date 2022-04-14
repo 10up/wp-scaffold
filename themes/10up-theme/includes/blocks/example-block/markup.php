dd282918b5bfd6eb7baeef3b58a2501de809947b
Fabian Kägy <mail@fabian-kaegy.de>
2022-04-14 17:21:51 +0200

    fix remove reference of non existent variable class_name in block markup

--- themes/10up-theme/includes/blocks/example-block/markup.php
+++ themes/10up-theme/includes/blocks/example-block/markup.php
@@ -8,12 +8,11 @@
  * @var string   $content            Block content.
  * @var WP_Block $block              Block instance.
  * @var array    $context            BLock context.
- * @var string   $class_name         Generated class name for concatenation.
  */
 
 ?>
 <div <?php echo get_block_wrapper_attributes(); // phpcs:ignore ?>">
-	<h2 class="<?php echo sanitize_html_class( $class_name ); ?>__title">
+	<h2 class="wp-block-tenup-example__title">
 		<?php echo wp_kses_post( $attributes['title'] ); ?>
 	</h2>
 </div>
