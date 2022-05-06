1b05a79e06013eabb6d4e2d4fecbd988084adb17
Joe Snell <joesnellpdx@gmail.com>
2022-05-06 16:53:46 -0700

    Updates to theme.json & button sink output - add text button to sink page - add outline and update active vars in theme.json

--- themes/10up-theme/assets/js/admin/admin.js
+++ themes/10up-theme/assets/js/admin/admin.js
@@ -1,3 +1,5 @@
+import '@10up/wp-scaffold-ui/assets/css/frontend/utilitystyles.css';
+import '@10up/wp-scaffold-ui/assets/css/frontend/componentstyles.css';
 import '../../css/admin/admin-style.css';
 
 // import foo from './bar'
