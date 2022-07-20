b329d3bec5724c3f2b7c43726504bf4fd8ef3e0d
Clayton Collie <clayton.collie@gmail.com>
2022-07-20 09:31:27 -0500

    Function wp_body_open() has no return type specified.

--- themes/10up-theme/functions.php
+++ themes/10up-theme/functions.php
@@ -40,6 +40,8 @@ if ( ! function_exists( 'wp_body_open' ) ) {
 
 	/**
 	 * Shim for the the new wp_body_open() function that was added in 5.2
+	 *
+	 * @return void
 	 */
 	function wp_body_open() {
 		do_action( 'wp_body_open' );
