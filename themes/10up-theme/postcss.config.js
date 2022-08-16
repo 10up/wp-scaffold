5a85a50c1b55cdd528084fdd0cd9b5730b8317be
Nikki <nikki.jones@get10up.com>
2022-08-16 11:09:58 -0700

    Add postcss modifications and stylintrc

--- themes/10up-theme/postcss.config.js
+++ themes/10up-theme/postcss.config.js
@@ -1,8 +1,27 @@
-module.exports = {
-	plugins: {
-		'postcss-import': {},
-		'tailwindcss/nesting': {},
-		tailwindcss: {},
-		autoprefixer: {},
-	},
+/* eslint-disable global-require */
+
+module.exports = () => {
+	const config = {
+		plugins: [],
+	};
+
+	// Only load postcss-editor-styles plugin when we're processing the editor-style.css file.
+
+	config.plugins.push(require('postcss-import'));
+	config.plugins.push(require('tailwindcss/nesting')('postcss-nesting'));
+	config.plugins.push(require('tailwindcss'));
+	config.plugins.push(
+		require('postcss-preset-env')({
+			stage: 0,
+			features: {
+				'custom-properties': false,
+				'nesting-rules': false,
+				'is-pseudo-class': {
+					onComplexSelector: 'ignore',
+				},
+			},
+		}),
+	);
+
+	return config;
 };
