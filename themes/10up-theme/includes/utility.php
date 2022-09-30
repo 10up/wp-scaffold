070ced5ec2577a79e3f60ec91735085d5d1826c4
Will Howat-Comandre <willhowat@gmail.com>
2022-09-30 10:34:39 +0200

    Updated get_color() to accept percentage values for RGBA and HSLA alpha channels

--- themes/10up-theme/includes/utility.php
+++ themes/10up-theme/includes/utility.php
@@ -47,9 +47,10 @@ function get_colors( $path ) {
 
 	if ( file_exists( $dir . $path ) ) {
 		$css_vars = file_get_contents( $dir . $path ); // phpcs:ignore WordPress.WP.AlternativeFunctions
-		preg_match_all( ' /#([a-f]|[A-F]|[0-9]){3}(([a-f]|[A-F]|[0-9]){3})?\b/', $css_vars, $matches );
-		// HEX | RGB(A) | HSL(A)
-		preg_match_all( '(#([\da-f]{3}){1,2}|(rgb|hsl)a\((\d{1,3}%?,\s?){3}(1|0?\.\d+)\)|(rgb|hsl)\(\d{1,3}%?(,\s?\d{1,3}%?){2}\))', $css_vars, $matches );
+		// HEX | RGB(A) | HSL(A) - alpha as decimal or percentage
+		// https://regex101.com/r/FEtzDu/1
+		preg_match_all( '(#(?:[\da-f]{3}){1,2}$|^#(?:[\da-f]{4}){1,2}$|(rgb|hsl)a?\((\s*-?\d+%?\s*,){2}(\s*-?\d+%?\s*)\)|(rgb|hsl)a?\((\s*-?\d+%?\s*,){3}\s*(0|(0?\.\d+)|1|(\s*\d{1,3}%\s*))\))', $css_vars, $matches );
+
 		return $matches[0];
 	}
 
