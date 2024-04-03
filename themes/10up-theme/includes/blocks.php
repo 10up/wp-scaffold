8d3682214ee996e82d00960bb95102b6799fd4bb
Fabian Kägy <mail@fabian-kaegy.de>
2024-04-03 14:36:30 +0200

    fix remove reference of ols block library filter

--- themes/10up-theme/includes/blocks.php
+++ themes/10up-theme/includes/blocks.php
@@ -24,18 +24,6 @@ function setup() {
 	add_action( 'init', $n( 'register_theme_blocks' ) );
 
 	add_action( 'init', $n( 'register_block_pattern_categories' ) );
-
-	/*
-	If you are using the block library, remove the blocks you don't need.
-
-	add_filter( 'tenup_available_blocks', function ( $blocks ) {
-		if ( ! empty( $blocks['integrated-hero'] ) ) {
-			unset( $blocks['integrated-hero'] );
-		}
-
-		return $blocks;
-	} );
-	*/
 }
 
 /**
