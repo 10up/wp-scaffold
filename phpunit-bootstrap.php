f934517de3bcc7100be497fef4da45fe41d51fbc
Daryll Doyle <daryll.doyle@10up.com>
2024-08-14 00:54:35 +0100

    Do we need to load the plugin?

--- phpunit-bootstrap.php
+++ phpunit-bootstrap.php
@@ -25,15 +25,6 @@ if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
 // Give access to tests_add_filter() function.
 require_once $_tests_dir . '/includes/functions.php';
 
-/**
- * Manually load the plugin being tested.
- */
-function _manually_load_plugin() {
-	require __DIR__ . '/mu-plugins/10up-plugin/plugin.php';
-}
-
-tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
-
 // Start up the WP testing environment.
 require $_tests_dir . '/includes/bootstrap.php';
 
